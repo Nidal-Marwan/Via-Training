27e4b2b9c9a9a44e8658bf498c8b20c69ff1c751
nidalhjab <nidal_hjab@hotmail.com>
2022-06-09 13:26:58 +0300

    feat: add new command for prod mode

--- packages/server/app.ts
+++ packages/server/app.ts
@@ -1,1 +1,14 @@
-console.log("inside server");
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+if (process.env.NODE_ENV) {
+  app.use(express.static(path.join(__dirname, "build")));
+} else {
+  app.use(express.static(path.join(__dirname, "public")));
+}
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
