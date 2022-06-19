275c29d0154effb8f8f3704d0318c074f6d2826f
nidalhjab <nidal_hjab@hotmail.com>
2022-06-19 15:59:30 +0300

    feat: add react routes

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -1,12 +1,11 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-
 import i18next from "i18next";
 import { initReactI18next } from "react-i18next";
 import HttpApi from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 import languages from "./common/localization/languages";
 import { App } from "./App";
+import { BrowserRouter } from "react-router-dom";
 
 i18next
   .use(HttpApi)
@@ -31,7 +30,7 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
