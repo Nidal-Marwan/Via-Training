26c68e438ba7e77e89a421cecafe93d2d1dc64fd
nidalhjab <nidal_hjab@hotmail.com>
2022-06-20 14:48:39 +0300

    Use custom theme (#14)
    * refactor

---
+++ packages/client/src/common/utils/theme.ts
@@ -0,0 +1,17 @@
+import { createTheme } from "@mui/material";
+
+export const customTheme = createTheme({
+  palette: {
+    primary: {
+      main: "#2e7d32 ",
+    },
+    secondary: {
+      main: "#DEC19B",
+    },
+    tonalOffset: 0.2,
+  },
+  typography: {
+    fontFamily: "sans-serif",
+    fontSize: 12,
+  },
+});
