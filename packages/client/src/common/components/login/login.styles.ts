711555192988d57eeb8f761a5e0bf94130a141d5
Mohannad Jamal <jamalmohannad@gmail.com>
2022-06-23 10:37:26 +0300

    Added border

--- packages/client/src/common/components/login/login.styles.ts
+++ packages/client/src/common/components/login/login.styles.ts
@@ -2,10 +2,13 @@ import { styled } from '@mui/system';
 import { Form } from 'formik';
 
 export const StyledForm = styled(Form)(({ theme }) => ({
+	padding: 30,
+	border: `1px solid ${theme.palette.primary.main}`,
+	borderRadius: 5,
 	marginTop: 20,
 	display: 'flex',
 	flexDirection: 'column',
-	width: 300,
+	width: 400,
 	gap: 25,
 	'& .MuiButton-root': {
 		width: 80,
