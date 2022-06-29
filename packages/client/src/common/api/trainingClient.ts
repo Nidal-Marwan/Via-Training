86349a0681a05ee70358485b76fef62843bc7b71
mohannadjamal <jamalmohannad@gmail.com>
2022-06-29 10:48:43 +0300

    BE Locations API + Interceptor
    * BE locations api
    
    * BE locations api
    
    * BE Location API
    
    * BE Locations API
    
    * BE Locations API
    
    * Fixed requests
    
    * Added intercepter
    
    * Removed headers from useMe hook

--- packages/client/src/common/api/trainingClient.ts
+++ packages/client/src/common/api/trainingClient.ts
@@ -1,5 +1,13 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 
 export const trainingClient = axios.create({
 	baseURL: "http://localhost:3001/api",
 });
+
+trainingClient.interceptors.request.use(async (config: AxiosRequestConfig) => {
+	if (config.url?.includes("user") || config.url?.includes("locations")) {
+		const token = window.localStorage.getItem("access_token");
+		config.headers!.Authorization = `Bearer ${token}`;
+	}
+	return config;
+}, error => Promise.reject(error));
