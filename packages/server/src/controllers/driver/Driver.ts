87d134927a3bea8b44c3c5cade1af0c4c6cbf5a8
nidalhjab <nidal_hjab@hotmail.com>
2022-07-17 11:48:37 +0300

    refactor: delete unnecessary requiered fields

--- packages/server/src/controllers/driver/Driver.ts
+++ packages/server/src/controllers/driver/Driver.ts
@@ -1,12 +1,9 @@
 export interface DriverData {
   id?: number;
   name: string;
-  lat: number | null;
-  lng: number | null;
   phone: string;
   carModel: string;
   licensePlate: string;
-  date: Date;
   locationId: number;
   userId: number;
 }
