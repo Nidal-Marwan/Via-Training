c0b397ad6419879e714e603121dbbd6037b8efc8
Mohannad Jamal <jamalmohannad@gmail.com>
2022-07-21 13:34:03 +0300

    Logo is clickable

--- packages/client/src/common/components/NavBar/Logo/Logo.tsx
+++ packages/client/src/common/components/NavBar/Logo/Logo.tsx
@@ -1,6 +1,13 @@
+import { Link } from "react-router-dom";
 import logo from "../../../../assets/images/logo192.png";
 
-const Logo = () => {
-	return <img src={logo}/>;
+interface LogoProps {
+	isLoggedIn: boolean;
+}
+
+const Logo = ({ isLoggedIn }: LogoProps) => {
+	return <Link to={isLoggedIn ? "live-map" : "/"}>
+		<img src={logo} />
+	</Link>;
 };
 export default Logo;
